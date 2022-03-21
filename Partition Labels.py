8b1944755e49bd92c7fc03de85c738426bcbf0e9
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-03-21 16:25:32 -0700

    Add files via upload

--- Partition Labels.py
+++ Partition Labels.py
@@ -24,3 +24,19 @@ class Solution:
                 res.append(size)
                 size = 0
         return res
+
+        # Solution #2
+
+        L = len(s)
+        last = {s[i]: i for i in range(L)} # last appearance of the letter
+        i, ans = 0, []
+        while i < L:
+            end, j = last[s[i]], i + 1
+            while j < end: # validation of the part [i, end]
+                if last[s[j]] > end:
+                    end = last[s[j]] # extend the part
+                j += 1
+            ans.append(end - i + 1)
+            i = end + 1
+            
+        return ans
