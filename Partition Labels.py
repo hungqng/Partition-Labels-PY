9eefd8b6a184e085dbf4591b284b59cc06fce08f
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-03-21 16:26:16 -0700

    Add files via upload

--- Partition Labels.py
+++ Partition Labels.py
@@ -27,16 +27,16 @@ class Solution:
 
         # Solution #2
 
-        L = len(s)
-        last = {s[i]: i for i in range(L)} # last appearance of the letter
-        i, ans = 0, []
-        while i < L:
-            end, j = last[s[i]], i + 1
-            while j < end: # validation of the part [i, end]
-                if last[s[j]] > end:
-                    end = last[s[j]] # extend the part
-                j += 1
-            ans.append(end - i + 1)
-            i = end + 1
+        # L = len(s)
+        # last = {s[i]: i for i in range(L)} # last appearance of the letter
+        # i, ans = 0, []
+        # while i < L:
+        #     end, j = last[s[i]], i + 1
+        #     while j < end: # validation of the part [i, end]
+        #         if last[s[j]] > end:
+        #             end = last[s[j]] # extend the part
+        #         j += 1
+        #     ans.append(end - i + 1)
+        #     i = end + 1
             
-        return ans
+        # return ans
